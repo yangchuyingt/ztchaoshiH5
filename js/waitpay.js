2196e82785b76d2a3d8f91287dbbbc1703bfba78
yangchuyingt <1191378206@qq.com>
2015-09-23 12:12:24 +0800

    resoloved

--- js/waitpay.js
+++ js/waitpay.js
@@ -1,16 +1,21 @@
-function showtitlenme {
+function showtitlenme(){
+	console.log("df:"+pagestatus)
 	switch (pagestatus) {
-		case 0:
-		  $('.me-header-title').text()="待付款"；
+		case '0':
+		console.log(0);
+		  $('.me-header-title').text("待付款");
 			break;
-		case 1:
-		  $('.me-header-title').text()="待收货"；
+		case '1':
+		console.log(1);
+		  $('.me-header-title').text("待收货");
 			break;
-		case 2:
-		 $('.me-header-title').text()="待评价"；
+		case '2':
+		console.log(2);
+		 $('.me-header-title').text("售后");
 			break;
-		case 3:
-		 $('.me-header-title').text()="售后"；
+		case '3':
+		console.log(3);
+		 $('.me-header-title').text()="售后";
 			break;
 	}
 }
