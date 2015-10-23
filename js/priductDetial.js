7642ac493facf3cb1f77d155ff600ce07a8895bd
yangchuyingt <1191378206@qq.com>
2015-10-23 18:35:42 +0800

    product size hava finished ,productparamter hava finished

--- js/priductDetial.js
+++ js/priductDetial.js
@@ -9,12 +9,13 @@ function getproductimg(id) {
 		$.post(url, {
 			"tmid": id,
 		}, function(result) {
-			console.log(JSON.stringify(result));
+			console.log("jj:"+JSON.stringify(result));
 			if(result.ret=='1'){
 				//	console.log("结果："+result.data.photos);
 			var picturls = result.data.photos.split("||");
 			//console.log(picturls.length);
 			setimgviews(picturls);
+			setproductparamters(result.data.param);
            $("#product-paramters").append(result.data.param);
           // console.log(JSON.stringify(result.data.param));
 			}else{
@@ -23,6 +24,10 @@ function getproductimg(id) {
 		}, "json");
 
 	}
+function setproductparamters(param){
+	$('.productmsg').empty();
+	$(".productmsg").append(param);
+}
 	/*
 	 *给产品详情界面加载轮播图
 	 *
@@ -75,15 +80,32 @@ function getproductchima(tmid){
 	$.post(url,{
 		"tmid":tmid
 	},function(result){
+		haveloadsize=true
 		console.log('尺码信息：'+JSON.stringify(result));
 		if(result.ret=="0"){
-			
-		}else{
+			havesize=false;
+		}else if(result.ret=='1'){
+			handsizemsg(result.data);
 			
 			//$("#size-masage").append(result.data.content);
 		}
-		
+		mui.fire(parentpage,'isloadsizemsg',{
+			'haveloadsize':haveloadsize,
+			"havesize":havesize
+		})
 	},"json")
+}
+/**
+ * <div class="product-size" >粉红白皮</div>
+ *  处理尺码信息
+ * */
+function handsizemsg(data){
+	var sizemsg='';
+	for(var i=0;i<data.length;i++){
+		sizemsg+='<div value="'+data[i].tmcid+'"class="product-size-unselect" >'+data[i].chima+'</div>';
+	}
+	$(".show-content").empty();
+	$(".show-content").append(sizemsg);
 }
 function showproductmsg(result){
 	$("#before-price").text("原价: ￥"+result.data.dj0);
@@ -98,6 +120,7 @@ function gotoorderpostpage(tmid){
 					styles: {},
 					extras: {
 						"tmid": tmid,
+						"productsize":selectedsize
 					},
 				});
 }
@@ -111,6 +134,7 @@ function addtoshopcart(tmid){
 		"sessid": sessid,
 		'tmid':tmid,
 		 tmsl:1,
+		 "cmid":selectedsize,
 		 'btimes':'0',
 		 'bnums':'0'
 	},function(result){
